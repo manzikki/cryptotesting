925a90e99a718d2d2a62e1bfd5b8b6d630fa588e
Standard Marko <standardmarko@MACBOOKs-MacBook-Air.local>
2018-05-14 15:24:09 +0700

    example updated

--- UNmaster/send-example.sh
+++ UNmaster/send-example.sh
@@ -1,2 +1,2 @@
-ultranotewallet --wallet-file wallet --password password --command transfer 0 Xun3UYpn6paWPU5qk9RneU6RGGgDb27jXESZU28t4U6vdCn9wjWihdGQWMHLfzL6vv6x454o4uP1RZuQkRNTcC548DsCX75d2R 0.1
-
+#ultranotewallet --wallet-file wallet --password password --command transfer 0 Xun3UYpn6paWPU5qk9RneU6RGGgDb27jXESZU28t4U6vdCn9wjWihdGQWMHLfzL6vv6x454o4uP1RZuQkRNTcC548DsCX75d2R 0.1
+ultranotewallet --nfc="123456789=Xun3UYpn6paWPU5qk9RneU6RGGgDb27jXESZU28t4U6vdCn9wjWihdGQWMHLfzL6vv6x454o4uP1RZuQkRNTcC548DsCX75d2R=0.1" --wallet-file=wallet --password=password
