4d965479a78a2d37994259eb3ed337c16ab4664a
Standard Marko <standardmarko@MACBOOKs-MacBook-Air.local>
2018-05-14 11:18:34 +0700

    adapted to latest May 2018

---
+++ UNmaster/send-example.sh
@@ -0,0 +1,2 @@
+ultranotewallet --wallet-file wallet --password password --command transfer 0 Xun3UYpn6paWPU5qk9RneU6RGGgDb27jXESZU28t4U6vdCn9wjWihdGQWMHLfzL6vv6x454o4uP1RZuQkRNTcC548DsCX75d2R 0.1
+
---
+++ UNmaster/src/P2p/P2pNetworks.h
@@ -0,0 +1,13 @@
+// Copyright (c) 2011-2017 The Cryptonote developers
+// Copyright (c) 2014-2017 XDN developers
+// Copyright (c) 2016-2017 BXC developers
+// Copyright (c) 2017 UltraNote developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+namespace CryptoNote
+{
+  const static boost::uuids::uuid CRYPTONOTE_NETWORK = { {  0x36, 0x22, 0xA1, 0x90, 0xC7, 0x5F, 0x82, 0xA7, 0xF5, 0x12, 0xA3, 0x36, 0xE7, 0xE3, 0xDD, 0x12 } };
+}
